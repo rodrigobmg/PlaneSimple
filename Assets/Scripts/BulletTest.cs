5de80856a086310be7ae2b13646166d712dd8a01
Christian Borst <christianoborst@gmail.com>
2017-02-01 09:31:28 -0600

    Plane and bullets correctly positioned in the world, preliminary firing mechanism in the works

---
+++ Assets/Scripts/BulletTest.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletTest : MonoBehaviour {
+
+    public Transform BulletPosition;
+    public GameObject bullets;
+    public float fireDelay = 0f;
+    private GameObject currBullet;
+    private float delay = 0f;
+	// Use this for initialization
+	void Start () {
+        if (BulletPosition == null)
+        {
+            Debug.LogWarning("The BulletPosition transform has not been assigned in the editor!");
+            return;
+        }
+        if (bullets == null)
+        {
+            Debug.LogWarning("The bullets object has not been assigned in the editor!");
+            return;
+        }
+        if (fireDelay == 0f)
+        {
+            fireDelay = 0.25f;
+        }
+        currBullet = Instantiate(bullets,BulletPosition);
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1") && delay <= 0)
+        {
+            Debug.Log("Fire~~~");
+            Fire();
+        }
+        if (delay >= 0f) //Always remove the delay to fire per frame
+        {
+            delay -= Time.deltaTime;
+        }
+	}
+
+    void Fire()
+    { 
+        currBullet.GetComponent<FireBullet>().Fire();
+        delay = fireDelay; //Reset the delay to next bullet firing
+    }
+}
---
+++ Assets/Scripts/FireBullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBullet : MonoBehaviour {
+
+    public float firingSpeed = 0f; // The speed the bullet moves at once fired
+    public float TTL = 5f; // Lifetime of the bullet once fired in seconds
+
+    private Rigidbody rb;
+    private bool firing = false;
+
+    public void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    public void Fire()
+    {
+        Debug.Log("Firing!~~~");
+        if (!firing)
+        {
+            firing = true;
+            rb.velocity = (transform.forward * firingSpeed);
+            Destroy(gameObject, TTL);
+        }
+    }
+}
